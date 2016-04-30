36cf5d2b55c646882bb655172b901e83c193782f
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-04-30 14:52:25 +0800

    Bump version to 0.1.0

--- apps/wankrank/mix.exs
+++ apps/wankrank/mix.exs
@@ -3,7 +3,7 @@ defmodule Wankrank.Mixfile do
 
   def project do
     [app: :wankrank,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
