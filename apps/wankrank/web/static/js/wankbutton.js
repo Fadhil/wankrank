01366bf814d839d20d90df55c2873cd491019a3a
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-04-30 14:30:22 +0800

    Use hide() instead of fadeOut(0) for wankbutton

--- apps/wankrank/web/static/js/wankbutton.js
+++ apps/wankrank/web/static/js/wankbutton.js
@@ -1,7 +1,7 @@
 export var Wankbutton = {
   updateWank: function(payload){
     let wankcount= $('span [data-video-id='+ payload.video_id +']')
-    wankcount.fadeOut(0)
+    wankcount.hide()
     wankcount.html(payload.wank_count).fadeIn()
     // wankcount.delay(100).fadeIn()
     console.log("Video " + payload.video_id + " clicked " + payload.wank_count + " times")
