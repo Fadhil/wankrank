49967acf2e7dd2dc8e459c4f6092f02a9052c1b8
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-05-20 12:10:40 +0800

    Add failing test for NameGenerator
    * Basic test to see if we can generate a name for our user

---
+++ apps/wankrank/test/lib/name_generator_test.exs
@@ -0,0 +1,9 @@
+defmodule NameGeneratorTest do
+  use ExUnit.Case, async: true
+
+  test "returns a random username made of three words and a number" do
+    # Three words CamelCased
+    regex = ~r([A-Z]\w*[A-Z]\w*[A-Z]\w*-\d{4})
+    assert Regex.match?(regex, NameGenerator.generate)
+  end
+end
