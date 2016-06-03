c5a02dd3b6054be7cefb14a8e0047c0c63594a21
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-06-03 15:00:13 +0800

    Refactor wankbutton.js
    * Define updateWank function separately
    * Easier to see what functions we've exported in `Wankbutton` module

--- apps/wankrank/web/static/js/wankbutton.js
+++ apps/wankrank/web/static/js/wankbutton.js
@@ -1,9 +1,11 @@
-export var Wankbutton = {
-  updateWank: function(payload){
+function updateWank(payload){
     let wankcount= $('span [data-video-id='+ payload.video_id +']')
     wankcount.hide()
     wankcount.html(payload.wank_count).fadeIn()
     // wankcount.delay(100).fadeIn()
     console.log("Video " + payload.video_id + " clicked " + payload.wank_count + " times")
   }
+
+export var Wankbutton = {
+  updateWank: updateWank
 }
